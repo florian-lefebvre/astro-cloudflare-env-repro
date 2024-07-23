b2a80c1cb1ee735483b07b9b7b24b97a4e72dfe2
florian-lefebvre <contact@florian-lefebvre.dev>
2024-07-23 15:44:49 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig, envField } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    env: {
+      schema: {
+        FOO: envField.string({
+          context: 'server',
+          access: 'secret',
+        }),
+      },
+      // validateSecrets: true,
+    },
+  },
+  output: 'server',
+  adapter: cloudflare(),
+});
