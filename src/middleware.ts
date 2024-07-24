2f2ebf3c2c8ba6920eab93825be9ddb462cbddcd
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-07-24 15:39:22 +0200

    don't call FOO outside request

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from "astro/middleware";
 import { FOO } from "astro:env/server";
 import { whatever } from "./lib";
 
-console.log('Hi from outside request', { FOO })
+// console.log('Hi from outside request', { FOO })
 
 const fn = () => {
   console.log("Hi from fn called in middleware", { FOO });
