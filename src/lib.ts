b4081db99869ec7f563b3d9aaf3192a21440e556
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-07-24 15:41:37 +0200

    tets

--- src/lib.ts
+++ src/lib.ts
@@ -1,9 +1,9 @@
-// import { FOO } from "astro:env/server";
+import { FOO } from "astro:env/server";
 
 export const whatever = () => {
   console.log("whatever");
 };
 
 export const logFoo = () => {
-  // console.log("Hi from logFoo", { FOO });
+  console.log("Hi from logFoo", { FOO });
 };
